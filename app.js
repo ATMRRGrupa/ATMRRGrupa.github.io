66b89ecf7011253339a6a2002f3781d27dfa675a
ATMRRGrupa <atmrr.grupa@gmail.com>
2026-01-28 19:36:07 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import Hls from "hls.js";
 
-const STREAM_URL = "https://baking-teach-bloom-present.trycloudflare.com/ATMRR/index.m3u8";
+const STREAM_URL = "https://vector-innocent-bailey-images.trycloudflare.com/ATMRR/index.m3u8";
 
 const video = document.getElementById("video");
 const playBtn = document.getElementById("playBtn");
